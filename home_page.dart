d150c610694d0cad82b80413c4b1a196ffab7739
Shehan - karunarathne <87872258+shehan-karunarathna@users.noreply.github.com>
2023-07-14 11:42:06 +0530

    Create home_page.dart

---
+++ home_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+        backgroundColor: Color.fromARGB(255, 54, 26, 214),
+      ),
+      body: Center(
+        child: Text(
+          'Welcome to the Home Page!',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
